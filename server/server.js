ede180adb24d8ce1c79225e8f3db44bdc5b0f83d
Aniruddha Ganguly <1605031@ugrad.cse.buet.ac.bd>
2020-02-01 21:58:15 +0600

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,9 +14,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'AGS',
+    text: 'dasryt niBBa',
+    create: 123
+  });
+
+  socket.on('createMessage', (message) => {
+    socket.emit('newMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
+
+
 });
 
 server.listen(port, () => {
