359f4bc8907662a52161f2af834d5746ad5ffb9a
Aniruddha Ganguly <1605031@ugrad.cse.buet.ac.bd>
2020-02-01 22:29:50 +0600

    Create new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,12 +14,29 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'ADMIN',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'ADMIN',
+    text: 'New user joined the chat app',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
