914541a12ba34a88e14b81d4d9c9d452809eb6e6
Aniruddha Ganguly <1605031@ugrad.cse.buet.ac.bd>
2020-02-01 22:13:37 +0600

    emit new message on create message

--- server/server.js
+++ server/server.js
@@ -14,14 +14,12 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'AGS',
-    text: 'dasryt niBBa',
-    create: 123
-  });
-
   socket.on('createMessage', (message) => {
-    socket.emit('newMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
