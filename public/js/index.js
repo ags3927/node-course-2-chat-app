7e82ae49aa47fa06a37bff7b7343143f31e15a6c
Aniruddha Ganguly <1605031@ugrad.cse.buet.ac.bd>
2020-02-03 22:59:46 +0600

    Add Mustache.js for message template

--- public/js/index.js
+++ public/js/index.js
@@ -13,37 +13,27 @@ socket.on('newEmail', function (email) {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('New message', message);
+  var template = jQuery('#message-template').html();
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} [${formattedTime}]: ${message.text}`);
-  jQuery('#messages').append(li);
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
+  var template = jQuery('#location-message-template').html();
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My Current Location</a>');
-  li.text(`${message.from} [${formattedTime}]: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
+  
 });
-
-// var sendButton = jQuery(`#send-message`);
-//
-// sendButton.on('click', function (e) {
-//   var text = jQuery('[name=message]').val();
-//   if (text) {
-//     socket.emit('createMessage', {
-//       from: 'User',
-//       text
-//     }, function () {
-//
-//     });
-//   }
-// })
-
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
